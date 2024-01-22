a9dcf7aecf6032a9f35968bdbde6d4ae866b2ca1
ssairaj99 <ssairajsahoo23@gmail.com>
2024-01-22 09:55:40 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+
+
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+from django.http import HttpResponse
+
+def registration(request):
+    ufo=UserForm()
+    pfo=ProfileForm()
+
+    d={'ufo':ufo,'pfo':pfo}
+
+    if request.method=='POST' and request.FILES:
+        ufd=UserForm(request.POST)
+        pfd=ProfileForm(request.POST,request.FILES)
+
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+
+            return HttpResponse('Registration Sucessful')
+        else:
+            return HttpResponse('Invalid Input')
+    return render(request,'registration.html',d)
